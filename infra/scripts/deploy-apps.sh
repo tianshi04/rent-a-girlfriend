561cc4fa7e3aafb9075bff6603f0ed85d7969ba2
tianshi04 <zomodat123@gmail.com>
2026-06-24 02:39:08 +0700

    refactor(profile-service): use DATABASE_URL for database connection (closes #291)

--- infra/scripts/deploy-apps.sh
+++ infra/scripts/deploy-apps.sh
@@ -17,11 +17,7 @@ echo "Deploying profile-service..."
 helm upgrade --install profile-service services/profile-service/deployments/helm \
   -f services/profile-service/deployments/helm/values.dev.yaml \
   -f infra/k8s/envs/dev/profile-values.dev.yaml \
-  --set secrets.DB_HOST="postgres-postgresql.postgres.svc.cluster.local" \
-  --set secrets.DB_PORT="5432" \
-  --set secrets.DB_USER="postgres" \
-  --set secrets.DB_PASSWORD="mysecretpassword" \
-  --set secrets.DB_NAME="shared_dev_db" \
+  --set secrets.DATABASE_URL="postgresql+asyncpg://postgres:mysecretpassword@postgres-postgresql.postgres.svc.cluster.local:5432/shared_dev_db" \
   --set config.KAFKA_BROKERS="kafka-broker-headless.kafka.svc.cluster.local:9092" \
   --set config.S3_ENDPOINT_URL="http://minio.minio.svc.cluster.local:9000" \
   --set secrets.S3_ACCESS_KEY_ID="minio_admin" \
